9732e38215da05acc8ffdef7ac0cb4448eb1e9c0
06hoang05 <97800448+06hoang05@users.noreply.github.com>
2022-09-02 23:10:58 +0700

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+
+namespace C_BankApplication
+{
+    internal class Customer
+    {
+        private string name;
+        private int age;
+        private List<BankAccount> accounts;
+        public Customer(string name, int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = new List<BankAccount> ();
+        }
+    }
+}
